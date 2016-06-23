460f1ae6a0a71fb11e44f626ad700715ee2de24e
Nicolas Badia <badia.nicolas@gmail.com>
2016-06-23 10:13:32 +0200

    Fixes issues with inline editing if the cell content has been overridden
    Inline editing wasn’t working in the case where the label were deeper
    than one level because.
    Positionning were also sometimes erratic because of an issue with
    jQuery which I fixed in 1373.

--- views/table_row.js
+++ views/table_row.js
@@ -26,37 +26,6 @@ SC.TableRowView = SC.ListItemView.extend({
 
   displayProperties: ['width'],
 
-  exampleEditor: SC.InlineTextFieldView.extend({
-
-    positionOverTargetView: function(target, exampleFrame, elem, _oldExampleFrame, _oldElem) {
-      var targetLayout = target.get('layout'),
-          layout = {};
-
-      // Deprecates isCollection and pane arguments by fixing them up if they appear.
-      if (!SC.none(_oldExampleFrame)) {
-        exampleFrame = _oldExampleFrame;
-        elem = _oldElem;
-
-        // @if(debug)
-        SC.warn("Developer Warning: the isCollection and pane arguments have been deprecated and can be removed.  The inline text field will now position itself within the same parent element as the target, thus removing the necessity to calculate the position of the target relative to the pane.");
-        // @endif
-      }
-
-      // In case where the label is part of an SC.ListItemView, in the table...
-      if (exampleFrame && elem) {
-        var frame = SC.offset(elem, 'parent');
-        layout.top = targetLayout.top + frame.y - exampleFrame.height/2;
-        layout.left = targetLayout.left + frame.x + exampleFrame.x;
-        layout.height = exampleFrame.height;
-        layout.width = exampleFrame.width;
-      } else {
-        layout = SC.copy(targetLayout);
-      }
-
-      this.set('layout', layout);
-    }
-  }),
-
   /*
     @read-only
   */
@@ -64,8 +33,7 @@ SC.TableRowView = SC.ListItemView.extend({
     return this.getPath('displayDelegate.tableDelegate');
   }.property('displayDelegate').cacheable(),
 
-  // PUBLIC METHODS
-
+  
   render: function(context) {
     var tableDelegate = this.get('tableDelegate'),
         left = 3,
@@ -173,100 +141,42 @@ SC.TableRowView = SC.ListItemView.extend({
   },
 
 
+  // ..........................................................
   // LABEL EDTITING
+  //
 
-  // TODO only one cell can currently be edited.
   $label: function (evt) {
     var columns = this.getPath('displayDelegate.columns');
-    if (evt) {
 
+    if (evt) {
       this._$label = [];
       if (evt.clickCount === 2) {
         var label = this.$(evt.target),
-            //contentValueKey = SC.makeArray(this.get('contentValueKey'));
             editableKeys = this.get('editableKeys');
 
-        // var l = label.attr("class");
         // take the parent, as it indicates the column we are in
-        var p = label.parent().attr("class");
-        var match = /col-([0-9])/.exec(p);
+        var match = null,
+          parent = label,
+          maxIteration = 10;
+
+        while (!match && parent && maxIteration) {
+          match = /col-([0-9])/.exec(parent.attr("class"));
+          if (!match) parent = parent.parent();
+          maxIteration--;
+        }
         if (match) {
           // column number is in match[1]
           var colnr = parseInt(match[1], 10);
           var colname = columns[colnr].key;
           if (editableKeys.contains(colname)){
             this.contentValueKey = colname; // this will allow editing the right field
-            this._$label = label;
+            this._$label = parent;
           }
         }
       }
     }
 
     return this._$label;
-  },
-
-
-  /*
-    Configures the editor to overlay the label properly.
-  */
-  inlineEditorWillBeginEditing: function (editor, editable, value) {
-    var content   = this.get('content'),
-        del       = this.get('displayDelegate'),
-        labelKey  = this.getDelegateProperty('contentValueKey', del),
-        el        = this.$label(),
-        columns   = del.get('columns'),
-        validator = this.get('validator'),
-        f, v, offset, fontSize, top, lineHeight, escapeHTML,
-        lineHeightShift, targetLineHeight;
-
-    v = (labelKey && content) ? (content.get ? content.get(labelKey) : content[labelKey]) : content;
-
-    f = this.computeFrameWithParentFrame(null);
-
-    // if the label has a large line height, try to adjust it to something
-    // more reasonable so that it looks right when we show the popup editor.
-    lineHeight = this._oldLineHeight = el.css('lineHeight');
-    fontSize = el.css('fontSize');
-    top = this.$().css('top');
-
-    if (top) top = parseInt(top.substring(0, top.length - 2), 0);
-    else top = 0;
-
-    lineHeightShift = 0;
-
-    if (fontSize && lineHeight) {
-      targetLineHeight = fontSize * 1.5;
-      if (targetLineHeight < lineHeight) {
-        el.css({ lineHeight: '1.5' });
-        lineHeightShift = (lineHeight - targetLineHeight) / 2;
-      } else this._oldLineHeight = null;
-    }
-
-    // find cell offset. can perhaps be done quicker in a normal for loop.
-    var cellOffset = 0;
-    columns.find(function (c) {
-      if (c.get('key') === labelKey) return c;
-      else {
-        cellOffset += c.get('width');
-      }
-    });
-    el = el[0];
-    offset = SC.offset(el);
-    f.x = cellOffset; // difference with list item view, set the x to be the cell offset
-    f.y = offset.y + top + lineHeightShift;
-    f.height = el.offsetHeight;
-    f.width = el.offsetWidth;
-
-    escapeHTML = this.get('escapeHTML');
-
-    editor.set({
-      value: v,
-      exampleFrame: f,
-      exampleElement: el,
-      multiline: NO,
-      validator: validator,
-      escapeHTML: escapeHTML
-    });
   },
 
 
@@ -299,7 +209,11 @@ SC.TableRowView = SC.ListItemView.extend({
     return NO;
   },
 
+
+  // ..........................................................
   // CHECKBOX EDTITING
+  //
+
   updateCheckbox: function (jQuery, state) {
     var renderer = this.get('theme').checkboxRenderDelegate;
 
